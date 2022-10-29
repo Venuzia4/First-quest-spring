761a251e8998c904321938bbea9fd4267369c879
Babongui mabika venuza <vhenuzya@yahoo.fr>
2022-10-29 14:45:44 +0200

    first route with controller

---
+++ firstProjectSpring/src/test/java/com/wildcodeschool/firstProjectSpring/FirstProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.firstProjectSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
